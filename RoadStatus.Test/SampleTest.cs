bdf42bc6c91fb9fd29537888bcfc07e81a417f24
sasakura-sora <h34dphones@gmail.com>
2019-02-24 11:37:50 +0000

    Adding test project

---
+++ RoadStatus.Test/SampleTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RoadStatus.Test
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    public class SampleTest
+    {
+
+        [Test]
+        public void GivenTest_Integrates()
+        {
+            Assert.True(true);
+        }
+    }
+}
