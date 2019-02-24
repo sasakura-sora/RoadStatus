d4637f9a0ce5ce6a0cc88c2a83929654415b3ae6
sasakura-sora <h34dphones@gmail.com>
2019-02-24 11:24:37 +0000

    Add project files.

---
+++ RoadStatus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoadStatus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
