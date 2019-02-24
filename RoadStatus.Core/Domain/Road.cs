1a55191a3ee9556936bbb446511491b1fd13358e
sasakura-sora <h34dphones@gmail.com>
2019-02-24 12:03:15 +0000

    Completing road model

--- RoadStatus.Core/Domain/Road.cs
+++ RoadStatus.Core/Domain/Road.cs
@@ -1,23 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace RoadStatus.Core.Domain
 {
     public class Road
     {
+        private readonly string description;
+        private readonly string status;
         private readonly string name;
 
         public Road()
         {
             this.name = string.Empty;
+            this.status = string.Empty;
+            this.description = string.Empty;
         }
 
-        public Road(string name)
+        public Road(string name, string status, string description)
         {
             this.name = name;
+            this.status = status;
+            this.description = description;
         }
 
         public String Name { get { return name; } }
+
+        public String Status { get { return status; } }
+
+        public String Description { get { return description; } }
     }
 }
--- RoadStatus.Test/ModelTests.cs
+++ RoadStatus.Test/ModelTests.cs
@@ -13,7 +13,6 @@ namespace RoadStatus.Test
         [Test]
         public void GivenRoadModel_Exists()
         {
-
             var model = new RoadStatus.Core.Domain.Road();
 
             Assert.IsNotNull(model);
@@ -22,7 +21,6 @@ namespace RoadStatus.Test
         [Test]
         public void GivenDefaultModel_RoadName_IsUndefined()
         {
-
             var model = new RoadStatus.Core.Domain.Road();
 
             Assert.AreEqual(string.Empty, model.Name);
@@ -31,10 +29,48 @@ namespace RoadStatus.Test
         [Test]
         public void GivenNamedModel_RoadName_IsSet()
         {
+            const string Name = "A2";
 
-            var model = new RoadStatus.Core.Domain.Road("A2");
+            var model = new RoadStatus.Core.Domain.Road(Name, "", "");
 
-            Assert.AreEqual("A2", model.Name);
+            Assert.AreEqual(Name, model.Name);
+        }
+
+        [Test]
+        public void GivenDefaultModel_RoadStatus_IsUndefined()
+        {
+            var model = new RoadStatus.Core.Domain.Road();
+
+            Assert.AreEqual(string.Empty, model.Status);
+        }
+
+        [Test]
+        public void GivenNamedModel_RoadStatus_IsSet()
+        {
+            const string Status = "Good";
+
+            var model = new RoadStatus.Core.Domain.Road("", Status, "");
+
+            Assert.AreEqual(Status, model.Status);
+        }
+
+
+        [Test]
+        public void GivenDefaultModel_RoadDescription_IsUndefined()
+        {
+            var model = new RoadStatus.Core.Domain.Road();
+
+            Assert.AreEqual(string.Empty, model.Description);
+        }
+
+        [Test]
+        public void GivenNamedModel_RoadDescription_IsSet()
+        {
+            const string Description = "No Exceptional Delays";
+
+            var model = new RoadStatus.Core.Domain.Road("", "", Description);
+
+            Assert.AreEqual(Description, model.Description);
         }
     }
 }
